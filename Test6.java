df148aa789a7132afd60132d96a5b4c52c0bdb04
te06049 <58560099+te06049@users.noreply.github.com>
2019-12-05 23:53:17 +0900

    Create Test6.java

---
+++ Test6.java
@@ -0,0 +1,30 @@
+//문자열과 문자를 입력 받아, 해당 문자열에서 문자가 몇 번 검색되었는지 출력
+import java.util.Scanner;
+
+class Test6{
+	public static void main(String [] args){
+		String st1, st2;
+		char c;
+		int count=0;
+		
+		Scanner s = new Scanner (System.in);
+		
+		System.out.print("문자열: ");
+		st1 = s.nextLine();
+		
+		System.out.print("문자: ");
+		st2 = s.nextLine();
+		c = st2.charAt(0);
+		
+		for(int i = 0;i<st1.length();i++){
+			if(st1.charAt(i) == c){
+				count++;
+				System.out.printf("%d번째 ",i+1);
+			}
+		}
+		System.out.println();
+		System.out.println("문자열 \"" + st1 +"\"에서 문자 \'" + c + "\'는" + count +"번 검색되었습니다.");
+		
+		
+	}
+}
